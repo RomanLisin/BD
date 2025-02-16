8010657bdc78915c1bb104667e46d5d452ae479c
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2025-02-16 11:09:57 +1000

    add group NOT NULL

--- SQL/DDL/SQLQuery5 - CREATE DATABASE All_In_One.sql
+++ SQL/DDL/SQLQuery5 - CREATE DATABASE All_In_One.sql
@@ -2,7 +2,7 @@ USE master;
 GO
 
 CREATE DATABASE VPD_311_HOME
-ON --Определяем параметры файла базы данных
+ON 
 (
 	NAME		= 'VPD_311_HOME',
 	FILENAME	= 'D:\Microsoft SQL Server\MSSQL13.SQLEXPRESS\MSSQL\DATA\VPD_311_HOME.mdf',
@@ -10,7 +10,7 @@ ON --Определяем параметры файла базы данных
 	MAXSIZE		= 500MB,
 	FILEGROWTH	= 8MB
 )
-LOG ON --Определяем параметры файла журнала базы данных
+LOG ON 
 (
 	NAME		= 'VPD_311_HOME_Log',
 	FILENAME	= 'D:\Microsoft SQL Server\MSSQL13.SQLEXPRESS\MSSQL\DATA\VPD_311_HOME.ldf',
@@ -54,7 +54,7 @@ CREATE TABLE RequiredDisciplines
 CREATE TABLE TeachersDisciplinesRelation
 (
 	PRIMARY KEY (teacher, discipline),
-	teacher				INT			CONSTRAINT	FK_TDR_Teachers				FOREIGN KEY REFERENCES Teachers(teacher_id),
+	teacher				INT		CONSTRAINT	FK_TDR_Teachers				FOREIGN KEY REFERENCES Teachers(teacher_id),
 	discipline			SMALLINT	CONSTRAINT	FK_TDR_Disciplines			FOREIGN KEY REFERENCES Disciplines(discipline_id)
 );
 CREATE TABLE DisciplinesDirectionsRelation
@@ -69,7 +69,7 @@ CREATE TABLE Groups
 (
 	group_id			INT				PRIMARY KEY,
 	group_name			NVARCHAR(24)	NOT NULL,
-	direction			TINYINT		CONSTRAINT	FK_Groups_Directions		FOREIGN KEY	REFERENCES	Directions(direction_id)
+	direction			TINYINT			NOT NULL	CONSTRAINT	FK_Groups_Directions	FOREIGN KEY	REFERENCES	Directions(direction_id)
 );
 GO
 
@@ -80,7 +80,7 @@ CREATE TABLE Students
 	first_name			NVARCHAR(50)	NOT NULL,
 	middle_name			NVARCHAR(50)		NULL,
 	birth_date			DATE			NOT NULL,
-	[group]				INT		NULL CONSTRAINT	FK_Studenst_Groups			FOREIGN KEY	REFERENCES	Groups(group_id)
+	[group]				INT		NULL CONSTRAINT	FK_Studenst_Groups		FOREIGN KEY	REFERENCES	Groups(group_id)
 );
 GO
 
@@ -89,29 +89,29 @@ CREATE TABLE Schedule
 	lesson_id			BIGINT			PRIMARY KEY,
 	date				DATE			NOT NULL,
 	time				TIME(7)			NOT NULL,
-	[group]				INT				NOT NULL	CONSTRAINT	FK_Schedule_Groups			FOREIGN KEY REFERENCES	Groups(group_id),
+	[group]				INT			NOT NULL	CONSTRAINT	FK_Schedule_Groups			FOREIGN KEY REFERENCES	Groups(group_id),
 	discipline			SMALLINT		NOT NULL	CONSTRAINT	FK_Schedule_Disciplines		FOREIGN KEY REFERENCES	Disciplines(discipline_id),
-	teacher				INT				NOT NULL	CONSTRAINT	FK_Schedule_Teachers		FOREIGN KEY REFERENCES	Teachers(teacher_id),
+	teacher				INT			NOT NULL	CONSTRAINT	FK_Schedule_Teachers		FOREIGN KEY REFERENCES	Teachers(teacher_id),
 	[subject]			NVARCHAR(256)		NULL,
-	spent				BIT				NOT NULL
+	spent				BIT			NOT NULL
 );
 GO
 
 CREATE TABLE Grades
 (
 	PRIMARY KEY	(student,lesson),
-	student				INT			CONSTRAINT	FK_Grades_Students			FOREIGN KEY	REFERENCES	Students(student_id),
+	student				INT		CONSTRAINT	FK_Grades_Students			FOREIGN KEY	REFERENCES	Students(student_id),
 	lesson				BIGINT		CONSTRAINT	FK_Grades_Schedule			FOREIGN KEY	REFERENCES	Schedule(lesson_id),
-	present				BIT			NOT NULL,
-	grade_1				TINYINT		CONSTRAINT	CK_Grades_1					CHECK(grade_1 > 0 AND grade_1 <= 12),
-	grade_2				TINYINT		CONSTRAINT	CK_Grades_2					CHECK(grade_2 > 0 AND grade_2 <= 12)
+	present				BIT		NOT NULL,
+	grade_1				TINYINT		CONSTRAINT	CK_Grades_1				CHECK(grade_1 > 0 AND grade_1 <= 12),
+	grade_2				TINYINT		CONSTRAINT	CK_Grades_2				CHECK(grade_2 > 0 AND grade_2 <= 12)
 );
 
 CREATE TABLE Exams
 (
 	PRIMARY KEY	(student,discipline),
-	student				INT			CONSTRAINT	FK_Exams_Students			FOREIGN KEY	REFERENCES	Students(student_id),
+	student				INT		CONSTRAINT	FK_Exams_Students			FOREIGN KEY	REFERENCES	Students(student_id),
 	discipline			SMALLINT	CONSTRAINT	FK_Exams_Disciplines		FOREIGN KEY REFERENCES	Disciplines(discipline_id),
-	grade				TINYINT		CONSTRAINT	CK_Exams_Grade				CHECK(grade > 0 AND grade <=12)
+	grade				TINYINT		CONSTRAINT	CK_Exams_Grade			CHECK(grade > 0 AND grade <=12)
 );
 GO
