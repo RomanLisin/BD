511959a4fded25b48a987268463220b5d25e7f27
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2025-02-28 20:31:58 +1000

    Change Discipline Add

--- SQL/Transact-SQL/SQLQuery8 Schedile ChangeDisciplinEveryWeek.sql
+++ SQL/Transact-SQL/SQLQuery8 Schedile ChangeDisciplinEveryWeek.sql
@@ -16,6 +16,13 @@ PRINT (@numberLessons_disc1)
 DECLARE	@numberLessons_disc2	AS TINYINT		=	(SELECT number_of_lessons FROM Disciplines WHERE discipline_id = @discipline_2);
 PRINT (@numberLessons_disc2)
 
+DECLARE @max_value_numLess AS TINYINT;
+
+SET @max_value_numLess = CASE 
+						 WHEN @numberLessons_disc1 > @numberLessons_disc2 THEN @numberLessons_disc1
+						 ELSE @numberLessons_disc2
+						END;
+
 DECLARE @min_value_numLess AS TINYINT;
 
 SET @min_value_numLess = CASE 
@@ -27,6 +34,7 @@ DECLARE	@Teacher			AS SMALLINT		=	(SELECT teacher_id FROM Teachers WHERE first_n
 DECLARE	@start_date			AS	DATE		=	N'2024-09-01';
 DECLARE	@date				AS	DATE		=	@start_date;
 DECLARE	@time				AS	TIME(0)		=	N'12:00';
+DECLARE @max_num_less_id	AS TINYINT		=	(SELECT discipline_id FROM Disciplines WHERE number_of_lessons = @max_value_numLess);
 
 PRINT	(@group);
 PRINT	(@discipline);
@@ -36,40 +44,48 @@ PRINT	(@start_date);
 PRINT	(@time);
 PRINT	('===============================');
 
-DEClARE	@diff_lesson_number		AS	TINYINT		=	@numberLessons_disc1 - @numberLessons_disc2;
+--DEClARE	@diff_lesson_number		AS	TINYINT		=	@numberLessons_disc1 - @numberLessons_disc2;
 	
-IF  @diff_lesson_number >= 0
-BEGIN
+-- IF  @diff_lesson_number >= 0
+
+DECLARE @i AS TINYINT = 0;	
 
 
-	WHILE	(@numberLessons_disc2 >= 0)
+	WHILE	(@numberLessons_disc1 + @numberLessons_disc2 > 0)
 	BEGIN
-
-		INSERT Schedule
-				([group], discipline, teacher,[date],[time], spent)
-		VALUES  (@group,IIF (@numberLessons_disc2%2,@discipline_2,@discipline_1), @teacher, @date,  @time,IIF(@date < GETDATE(), 1,0));	
-		SET @numberLessons_disc2 = @numberLessons_disc2 - 1;
-
-		INSERT Schedule
-				([group], discipline, teacher,[date],[time], spent)
-		VALUES  (@group, IIF (@numberLessons_disc2%2,@discipline_2,@discipline_1), @teacher, @date, DATEADD(MINUTE, 95, @time), IIF(@date < GETDATE(), 1,0));	
-		SET @numberLessons_disc2 = @numberLessons_disc2 - 1;
-		INSERT Schedule
-				([group], discipline, teacher,[date],[time], spent)
-		VALUES  (@group, IIF (@numberLessons_disc2%2,@discipline_2,@discipline_1), @teacher, @date, DATEADD(MINUTE, 190, @time), IIF(@date < GETDATE(), 1,0));
-		SET @numberLessons_disc2 = @numberLessons_disc2 - 1;
-
+		WHILE (@i<=190)
+		BEGIN
+			IF	(@numberLessons_disc1 > 0 AND @numberLessons_disc2 > 0)
+			BEGIN
+				INSERT Schedule
+						([group], discipline, teacher,[date],[time], spent)
+				VALUES  (@group,IIF (@min_value_numLess % 2,@discipline_2,@discipline_1), @teacher, @date,  DATEADD(MINUTE, @i, @time),IIF(@date < GETDATE(), 1,0));	
+				SET @numberLessons_disc2 = @numberLessons_disc2 - 1;
+				SET @i = @i+95;
+			END
+			--INSERT Schedule
+			--		([group], discipline, teacher,[date],[time], spent)
+			--VALUES  (@group, IIF (@min_value_numLess % 2,@discipline_2,@discipline_1), @teacher, @date, DATEADD(MINUTE, 95, @time), IIF(@date < GETDATE(), 1,0));	
+			--SET @numberLessons_disc2 = @numberLessons_disc2 - 1;
+			--INSERT Schedule
+			--		([group], discipline, teacher,[date],[time], spent)
+			--VALUES  (@group, IIF (@min_value_numLess % 2,@discipline_2,@discipline_1), @teacher, @date, DATEADD(MINUTE, 190, @time), IIF(@date < GETDATE(), 1,0));
+			--SET @numberLessons_disc2 = @numberLessons_disc2 - 1;
+			IF (@numberLessons_disc1 > 0 OR @numberLessons_disc2 > 0) AND NOT (@numberLessons_disc1 > 0 AND @numberLessons_disc2 > 0)
+			BEGIN
+				INSERT Schedule
+						([group], discipline, teacher,[date],[time], spent)
+				VALUES  (@group,@max_num_less_id, @teacher, @date,  DATEADD(MINUTE, @i, @time),IIF(@date < GETDATE(), 1,0));	
+				SET @numberLessons_disc2 = @numberLessons_disc2 - 1;
+				SET @i = @i+95;
+			END
+		END
 	PRINT('---------------------------------');
 	SET @date = DATEADD(WEEK, 1, @date);
+	SET @numberLessons_disc1 = @numberLessons_disc1-1;
+	SET @numberLessons_disc2 = @numberLessons_disc2-1;
 	END
 		------------------------------------------------------------------------------------------
-		PRINT(@lesson_number+1);
-		PRINT(DATEADD(MINUTE, 95,@time));
+	
 
-		
-		SET @lesson_number = @lesson_number + 1;
 
-	END
-	PRINT('---------------------------------');
-	SET @date = DATEADD(DAY, 1, @date);
-END
